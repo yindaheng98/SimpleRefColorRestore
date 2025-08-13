2d61c47b176444a18aeaf1806cf722c5bb87debf
yindaheng98 <yindaheng98@gmail.com>
2025-08-12 21:03:07 -0700

    save image in different folder

--- refcolorrestore/dataset/abc.py
+++ refcolorrestore/dataset/abc.py
@@ -45,9 +45,15 @@ class RestorationDataset:
 
     def save_image_tuple(self, idx, image_dir):
         restoration, gt = self[idx]
-        torchvision.utils.save_image(restoration.color_distorted, os.path.join(image_dir, '{0:05d}'.format(idx) + ".png"))
-        torchvision.utils.save_image(restoration.reference, os.path.join(image_dir, '{0:05d}'.format(idx) + ".lr.png"))
-        torchvision.utils.save_image(gt, os.path.join(image_dir, '{0:05d}'.format(idx) + ".gt.png"))
+        color_distorted_dir = os.path.join(image_dir, 'distorted')
+        os.makedirs(color_distorted_dir, exist_ok=True)
+        torchvision.utils.save_image(restoration.color_distorted, os.path.join(color_distorted_dir, '{0:05d}'.format(idx) + ".png"))
+        reference_dir = os.path.join(image_dir, 'reference')
+        os.makedirs(reference_dir, exist_ok=True)
+        torchvision.utils.save_image(restoration.reference, os.path.join(reference_dir, '{0:05d}'.format(idx) + ".png"))
+        gt_dir = os.path.join(image_dir, 'groundtruth')
+        os.makedirs(gt_dir, exist_ok=True)
+        torchvision.utils.save_image(gt, os.path.join(gt_dir, '{0:05d}'.format(idx) + ".png"))
 
     def save_dataset(self, image_dir):
         os.makedirs(image_dir, exist_ok=True)
@@ -57,18 +63,20 @@ class RestorationDataset:
 
 class SavedRestorationDataset(RestorationDataset):
     def __init__(self, data_dir: str):
-        self.data_dir = data_dir
+        self.color_distorted_dir = os.path.join(data_dir, 'distorted')
+        self.reference_dir = os.path.join(data_dir, 'reference')
+        self.ground_truth_dir = os.path.join(data_dir, 'groundtruth')
 
     def __len__(self) -> int:
         n = 0
-        while os.path.exists(os.path.join(self.data_dir, '{0:05d}'.format(n) + ".png")) and \
-                os.path.exists(os.path.join(self.data_dir, '{0:05d}'.format(n) + ".lr.png")) and \
-                os.path.exists(os.path.join(self.data_dir, '{0:05d}'.format(n) + ".gt.png")):
+        while os.path.exists(os.path.join(self.color_distorted_dir, '{0:05d}'.format(n) + ".png")) and \
+                os.path.exists(os.path.join(self.reference_dir, '{0:05d}'.format(n) + ".png")) and \
+                os.path.exists(os.path.join(self.ground_truth_dir, '{0:05d}'.format(n) + ".png")):
             n += 1
         return n
 
     def __getitem__(self, idx) -> Tuple[RestorationTuple, torch.FloatTensor]:
-        color_distorted = torchvision.io.read_image(os.path.join(self.data_dir, '{0:05d}'.format(idx) + ".png")) / 255.0
-        reference = torchvision.io.read_image(os.path.join(self.data_dir, '{0:05d}'.format(idx) + ".lr.png")) / 255.0
-        ground_truth = torchvision.io.read_image(os.path.join(self.data_dir, '{0:05d}'.format(idx) + ".gt.png")) / 255.0
+        color_distorted = torchvision.io.read_image(os.path.join(self.color_distorted_dir, '{0:05d}'.format(idx) + ".png")) / 255.0
+        reference = torchvision.io.read_image(os.path.join(self.reference_dir, '{0:05d}'.format(idx) + ".png")) / 255.0
+        ground_truth = torchvision.io.read_image(os.path.join(self.ground_truth_dir, '{0:05d}'.format(idx) + ".png")) / 255.0
         return RestorationTuple(color_distorted, reference), ground_truth
